fc06bc607a439465bec32a5b1839b9b5e1466cce
ProgrammerShahJalal <you@example.com>
2023-11-04 16:17:53 +0600

    image display issue fixed

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w%ljtlal_p6eq!+4=g#e93p*8j4@^k#raqm^ma+zl5v2#l)%cq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
